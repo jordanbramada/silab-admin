fd4e71910979ca917113d61727432d7aa3d82b24
jordanbramada <mjordanbramada@gmail.com>
2024-11-14 15:21:16 +0700

    fix : optimizing pengumuman page to server side rendered

--- app/dashboard/pengumuman/list-pengumuman/page.tsx
+++ app/dashboard/pengumuman/list-pengumuman/page.tsx
@@ -1,44 +1,25 @@
-"use client";
-
 import { getAnnouncementList } from "@/app/actions/dashboard/pengumuman/actions";
+import AnnouncementCard from "@/app/components/announcement-card";
 import AnnouncementSettingsDropdownItem from "@/app/components/announcement-settings-dropdown-item";
+import { getAccessToken } from "@/app/lib/sessions";
 import { Pengumuman } from "@/app/types/pengumuman";
 import { PengumumanQuery } from "@/app/types/pengumuman-query";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 
-const pengumumanQuery: PengumumanQuery[] = [
-  { value: true, title: "Terposting" },
-  { value: false, title: "Belum Diposting" },
-  { value: null, title: "Semua" },
-];
-
-export default function ListPengumuman() {
-  const [announcementData, setAnnouncementData] = useState<Pengumuman[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setAnnouncementData([]);
-        setLoading(true);
-
-        const responseData = await getAnnouncementList();
-        if (responseData["status"] === "success") {
-          setAnnouncementData(responseData["data"]);
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
+export default async function ListPengumuman() {
+  const accessToken = await getAccessToken();
+  const res = await fetch(`${process.env.BASE_URL}/announcements`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    cache: "no-store",
+  });
+  const responseData = await res.json();
+  const data: Pengumuman[] = (await responseData["data"]) as Pengumuman[];
 
   return (
     <div className="flex h-full w-full flex-col overflow-auto overscroll-contain">
@@ -48,67 +29,12 @@ export default function ListPengumuman() {
         </p>
       </div>
       <div className="mt-10 flex flex-col space-y-10">
-        {loading && (
-          <span className="loading loading-dots loading-md self-center" />
-        )}
-        {!loading &&
-          announcementData.map((announcement) => (
-            <div
+        {data &&
+          data.map((announcement: Pengumuman) => (
+            <AnnouncementCard
+              announcement={announcement}
               key={announcement.id}
-              className="flex w-full flex-row space-x-3"
-            >
-              <div className="flex h-[210px] w-full flex-col space-y-8 rounded-2xl bg-white p-8 text-[#1d1d1d]">
-                <div className="flex flex-col">
-                  <p className="text-[22px] font-bold">{announcement.title}</p>
-                  <p className="text-[16px] font-light">
-                    {new Date(announcement.created_at).toUTCString()} by{" "}
-                    {announcement.author}
-                  </p>
-                </div>
-                <p className="text-[18px] font-semibold">{announcement.body}</p>
-              </div>
-              <Menu>
-                <MenuButton className="relative h-[24px] w-[24px]">
-                  <Image
-                    src={"/dots-vertical.png"}
-                    alt="announcement settings"
-                    className="static"
-                    style={{ objectFit: "contain" }}
-                    fill
-                    priority
-                  />
-                </MenuButton>
-                <MenuItems
-                  transition
-                  anchor="bottom end"
-                  className={`flex flex-col space-y-4 rounded-2xl bg-white p-4 shadow-[#1D1D1D]/10 drop-shadow-md`}
-                >
-                  <AnnouncementSettingsDropdownItem
-                    hoverIcon="/details-hovered.png"
-                    icon="/details.png"
-                    title="Lihat Detail"
-                    id={announcement.id}
-                    onMenuClick={(value) => router.push(`${value}`)}
-                  />
-                  <div className="h-[1px] w-full bg-[#1D1D1D]/10" />
-                  <AnnouncementSettingsDropdownItem
-                    hoverIcon="/edit-hovered.png"
-                    icon="/edit.png"
-                    title="Edit"
-                    id={announcement.id}
-                    onMenuClick={(value) => console.log(value)}
-                  />
-                  <div className="h-[1px] w-full bg-[#1D1D1D]/10" />
-                  <AnnouncementSettingsDropdownItem
-                    hoverIcon="/delete-hovered.png"
-                    icon="/delete.png"
-                    title="Hapus"
-                    id={announcement.id}
-                    onMenuClick={(value) => console.log(value)}
-                  />
-                </MenuItems>
-              </Menu>
-            </div>
+            />
           ))}
       </div>
     </div>
