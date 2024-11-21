1d323b8a3124f0b1d6f11ae1814ffd6ba719a35b
jordanbramada <mjordanbramada@gmail.com>
2024-11-21 13:16:50 +0700

    refactor: separate dashboard banner into its own component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import DashboardDataContainer from "../components/dashboard-data-container";
 import {
   getAllStudents,
@@ -6,6 +5,7 @@ import {
   getTotalRegisteredStudents,
   getUnpaidStudents,
 } from "../actions/dashboard/actions";
+import BannerDashboard from "../components/banner-dashboard";
 
 export default async function Dashboard() {
   const subjectData = await getTotalMatkul();
@@ -18,27 +18,10 @@ export default async function Dashboard() {
 
   return (
     <div className="flex h-full w-full flex-col justify-between space-y-6 overflow-auto overscroll-contain">
-      <div className="flex h-[300px] w-full flex-row items-start justify-between">
-        <div className="flex w-[534px] flex-col space-y-4">
-          <p className="text-[54px] font-bold leading-tight text-[#1d1d1d]">
-            Selamat datang di Dashboard{" "}
-            <span className="font-extrabold text-[#3272CA]">SILAB.</span>
-          </p>
-          <p className="text-[24px] font-semibold leading-tight text-[#5E6278]">
-            Atur dan pantau semua informasi praktikum dengan mudah di sini.
-          </p>
-        </div>
-        <Image
-          src={"illustration-1.svg"}
-          alt="illustration"
-          width={222}
-          height={300}
-          priority
-        />
-      </div>
+      <BannerDashboard />
       <div className="flex w-full flex-col space-y-5">
         <div className="mt-6 flex w-full flex-row space-x-5">
-          <DashboardDataContainer
+          {/* <DashboardDataContainer
             data={subjectData["data"].length}
             title="Jumlah Praktikum"
             subTitle="Semester 1 - 8"
@@ -54,7 +37,7 @@ export default async function Dashboard() {
             data2={studentsData["data"].length}
             title="Jumlah Mahasiswa"
             subTitle="Yang belum membayar praktikum"
-          />
+          /> */}
         </div>
       </div>
     </div>
