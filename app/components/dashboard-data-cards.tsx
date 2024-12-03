6a419962f69c5f69520e802f9525e421de15fcaa
jordanbramada <mjordanbramada@gmail.com>
2024-12-03 15:11:49 +0700

    feat: add assistant class data for practicum

--- app/components/dashboard-data-cards.tsx
+++ app/components/dashboard-data-cards.tsx
@@ -1,26 +1,53 @@
+import { getRole } from "../lib/sessions";
 import DashboardDataContainer from "./dashboard-data-container";
 
-export default function DashboardDataCards({subjectData, studentsData, studentsPaymentStatusPaidData, studentsPaymentStatusPendingData}: any) {
+export default async function DashboardDataCards({
+  subjectData,
+  studentsData,
+  studentsPaymentStatusPaidData,
+  studentsPaymentStatusPendingData,
+  studentPracticumData,
+}: any) {
+  const role = await getRole();
   return (
     <div className="flex w-full flex-col space-y-5">
       <div className="mt-6 flex w-full flex-row space-x-5">
-        <DashboardDataContainer
-          data={subjectData["data"].length}
-          title="Jumlah Praktikum"
-          subTitle="Semester 1 - 8"
-        />
-        <DashboardDataContainer
-          data={studentsPaymentStatusPaidData["data"].length}
-          data2={studentsData["data"].length}
-          title="Jumlah Mahasiswa"
-          subTitle="Yang sudah mendaftar praktikum"
-        />
-        <DashboardDataContainer
-          data={studentsPaymentStatusPendingData["data"].length}
-          data2={studentsData["data"].length}
-          title="Jumlah Mahasiswa"
-          subTitle="Yang belum membayar praktikum"
-        />
+        {role === "laborant" && (
+          <>
+            {" "}
+            <DashboardDataContainer
+              data={subjectData["data"].length}
+              title="Jumlah Praktikum"
+              subTitle="Semester 1 - 8"
+            />
+            <DashboardDataContainer
+              data={studentsPaymentStatusPaidData["data"].length}
+              data2={studentsData["data"].length}
+              title="Jumlah Mahasiswa"
+              subTitle="Yang sudah mendaftar praktikum"
+            />
+            <DashboardDataContainer
+              data={studentsPaymentStatusPendingData["data"].length}
+              data2={studentsData["data"].length}
+              title="Jumlah Mahasiswa"
+              subTitle="Yang belum membayar praktikum"
+            />
+          </>
+        )}
+        {role === "student" && (
+          <>
+            <DashboardDataContainer
+              data={subjectData["data"].length}
+              title="Jumlah Praktikum"
+              subTitle="Semester 1 - 8"
+            />
+            <DashboardDataContainer
+              data={studentPracticumData["data"].length}
+              title="Jumlah Kelas Asisten Praktikum"
+              subTitle="Semester 1 - 8"
+            />
+          </>
+        )}
       </div>
     </div>
   );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import {
   getTotalMatkul,
   getTotalRegisteredStudents,
   getUnpaidStudents,
+  getAllSubjectClasses,
 } from "../actions/dashboard/actions";
 import BannerDashboard from "../components/banner-dashboard";
 import DashboardDataCards from "../components/dashboard-data-cards";
@@ -12,6 +13,7 @@ export default async function Dashboard() {
   const studentsData = await getAllStudents();
   const studentsPaymentStatusPendingData = await getUnpaidStudents();
   const studentsPaymentStatusPaidData = await getTotalRegisteredStudents();
+  const studentPracticumData = await getAllSubjectClasses();
 
   return (
     <div className="flex h-full w-full flex-col justify-between space-y-6 overflow-auto overscroll-contain">
@@ -21,6 +23,7 @@ export default async function Dashboard() {
         studentsData={studentsData}
         studentsPaymentStatusPaidData={studentsPaymentStatusPaidData}
         studentsPaymentStatusPendingData={studentsPaymentStatusPendingData}
+        studentPracticumData={studentPracticumData}
       />
     </div>
   );
